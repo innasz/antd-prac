7321e739891f7ce6582074494ef94c8d67a89845
innasz <mini5167@naver.com>
2024-01-03 17:42:04 +0900

    style: Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Form, Input, Button } from "antd";
 
 function App() {
+  const onFinish = (e: any) => {
+    console.log(e);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form onFinish={onFinish}>
+          <Form.Item label="User Name" name="username">
+            <Input placeholder="User name" required></Input>
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input.Password
+              required
+              placeholder="Password"
+              maxLength={12}
+            ></Input.Password>
+          </Form.Item>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
